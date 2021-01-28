c90712c5fd01a52e47529e45714eb40025c2f006
Andre1312 <alcbarros@gmail.com>
2021-01-28 20:30:51 -0300

    Update Controllera
    Update Controller:
    -Categoria

--- src/main/java/com/prjlojaunitandrebarros/mvc/controller/CategoriaController.java
+++ src/main/java/com/prjlojaunitandrebarros/mvc/controller/CategoriaController.java
@@ -1,20 +1,24 @@
 package com.prjlojaunitandrebarros.mvc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import com.prjlojaunitandrebarros.mvc.model.Categoria;
 import com.prjlojaunitandrebarros.mvc.repository.CategoriaRepository;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path="/categoria")
 public class CategoriaController {
 	@Autowired
@@ -22,80 +26,54 @@ public class CategoriaController {
 
 	@PostMapping(path="/criar")
 	public @ResponseBody String criarCategoria (
-
-			@RequestParam 	String nome,
-							Boolean ativo
-			
-			) {
-
-
-		Categoria categoria = new Categoria();
-		categoria.setNome(nome);
-		categoria.setAtivo(ativo);
-
+			@RequestBody Categoria categoria)
+	{
 		categoriaRepo.save(categoria);
-		
-		
 		return "Categoria: Registro Criado\n";
-
 	}
 
 	@GetMapping(path="/listar")
-	public @ResponseBody Iterable<Categoria> getAllCategoria() {
-
+	public @ResponseBody Iterable<Categoria> listaCategoria() {
 		return categoriaRepo.findAll();
 	}
 
 	@DeleteMapping(path="/apagar/{id}")
-	public @ResponseBody String  apagarCategoria(@PathVariable Integer id) {
-
+	@ResponseStatus(code=HttpStatus.ACCEPTED)
+	public @ResponseBody String  apagarCategoria(@PathVariable Integer id)
+	{
 
 		if (categoriaRepo.findById(id) == null) {
 			return "ID " + id +" Não encontrado";
 		}
 
 		categoriaRepo.deleteById(id);
-
 		return "Categoria: Registro "+id+" Apagado";
 	}
 
-	@PutMapping(path="/alterar/{id}")
+	@PutMapping(path="/atualizar/{id}")
 	public @ResponseBody String alterarCategoria(
-			
 			@PathVariable Integer id,
-			
-			@RequestParam 	String nome,
-							boolean ativo
-			
-			) {
-
-
+			@RequestBody Categoria categoriaDetalhes)
+	{
 		if (categoriaRepo.findById(id) == null) {
 			return "ID " + id +" Não encontrado";
 		}
 
-		Categoria categoria = new Categoria();
+		Categoria categoria = categoriaRepo.findById(id).get();
 		
-		categoria.setNome(nome);
-		categoria.setAtivo(ativo);
+		categoria.setNome(categoriaDetalhes.getNome());
+		categoria.setAtivo(categoriaDetalhes.getAtivo());
 
 		categoriaRepo.save(categoria);
 		return "Categoria: Registro " +id+ " Alterado";
 	}
 	
 	@GetMapping(path="/buscar/{id}")
-	public @ResponseBody ResponseEntity<Categoria> getById(
-			
-		@PathVariable Integer id) {
-			
-		if (categoriaRepo.findById(id) == null) {
-			return (ResponseEntity<Categoria>) ResponseEntity.notFound();
-					
-					//.body("ID " + id +" Não encontrado");
-		}
-		
-		Categoria categoria = new Categoria();
-		return ResponseEntity.ok().body(categoria);
+	public @ResponseBody Categoria getById(
+			@PathVariable Integer id)
+	{
+
+		return categoriaRepo.findById(id).get();
 	}
 
 
